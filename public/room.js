7dba294ac271391701b7f4d64de12592d97786cb
Carl Xu <xu1743@purdue.edu>
2026-03-07 22:16:40 -0500

    even more changes

--- public/room.js
+++ public/room.js
@@ -103,23 +103,28 @@ function createPeerConnection() {
 
     const stats = await peerConnection.getStats();
     stats.forEach(report => {
-      if (report.type === 'inbound-rtp' && report.kind === 'video') {
+      if ((report.type === 'inbound-rtp' || report.type === 'outbound-rtp') && report.kind === 'video') {
         if (lastStats) {
           const prev = lastStats.get(report.id);
-          const deltaSeconds = (report.timestamp - prev.timestamp) / 1000;
-          const fps = (report.framesDecoded - prev.framesDecoded) / deltaSeconds;
-          
-          const currentLoss = report.packetsLost - prev.packetsLost;
-          
-          document.getElementById('val-fps').innerText = Math.round(fps);
-          document.getElementById('val-loss').innerText = currentLoss;
+          if (prev) {
+            const deltaT = (report.timestamp - prev.timestamp) / 1000;
+            
+            const frames = report.type === 'inbound-rtp' ? report.framesDecoded : report.framesEncoded;
+            const prevFrames = report.type === 'inbound-rtp' ? prev.framesDecoded : prev.framesEncoded;
+            
+            const fps = (frames - prevFrames) / deltaT;
+            
+            if (!isNaN(fps)) document.getElementById('val-fps').innerText = Math.round(fps);
+            
+            if (report.type === 'inbound-rtp') {
+              const currentLoss = report.packetsLost - prev.packetsLost;
+              document.getElementById('val-loss').innerText = currentLoss;
+            }
+          }
         }
       }
-
       if (report.type === 'candidate-pair' && report.state === 'succeeded') {
-        if (report.currentRoundTripTime !== undefined) {
-          document.getElementById('val-rtt').innerText = Math.round(report.currentRoundTripTime * 1000);
-        }
+        document.getElementById('val-rtt').innerText = Math.round(report.currentRoundTripTime * 1000);
       }
     });
     lastStats = stats;
